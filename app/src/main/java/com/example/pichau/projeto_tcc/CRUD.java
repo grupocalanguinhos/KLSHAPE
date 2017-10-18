aca3ffbe189fd2be5447b4cb7f803667947c426f
João Vitor Brandão <joaovitor.brandao21@gmail.com>
2017-10-18 21:15:04 -0200

    Atualização do Codigo

--- app/src/main/java/com/example/pichau/projeto_tcc/CRUD.java
+++ app/src/main/java/com/example/pichau/projeto_tcc/CRUD.java
@@ -13,13 +13,12 @@ public class CRUD {
         Banco = new Banco(context);
     }
 
-    public String inserir(String nome, String idade, String rg, String cpf, String endereco, String email, String senha ){
+    public String inserir(String nome, String rg, String cpf, String endereco, String email, String senha ){
         ContentValues valores;
         long resultado;
         db=Banco.getWritableDatabase();
         valores = new ContentValues();
         valores.put(Banco.NOME, nome);
-        valores.put(Banco.IDADE,idade);
         valores.put(Banco.RG,rg);
         valores.put(Banco.CPF,cpf);
         valores.put(Banco.ENDERECO,endereco);
--- app/src/main/java/com/example/pichau/projeto_tcc/CadastroInstrutor.java
+++ app/src/main/java/com/example/pichau/projeto_tcc/CadastroInstrutor.java
@@ -7,41 +7,39 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import com.example.pichau.projeto_tcc.CRUD;
-
 public class CadastroInstrutor extends AppCompatActivity {
 
-    EditText et_nome,et_idade, et_rg, et_cpf, et_endereco, et_email, et_senha;
+    EditText nm,rg,cpf,end,email,senha;
 
 
-    ImageButton btn4;
+    ImageButton btCad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_instrutor);
-        et_nome = (EditText) findViewById(R.id.et_nome);
-        et_idade = (EditText) findViewById(R.id.et_idade);
-        et_rg = (EditText) findViewById(R.id.et_rg);
-        et_cpf = (EditText) findViewById(R.id.et_cpf);
-        et_endereco = (EditText) findViewById(R.id.et_endereco);
-        et_email = (EditText) findViewById(R.id.et_email);
-        et_senha = (EditText) findViewById(R.id.et_senha);
-        btn4 = (ImageButton) findViewById(R.id.btn4);
+        nm = (EditText) findViewById(R.id.nm);
+        rg = (EditText) findViewById(R.id.rg);
+        cpf = (EditText) findViewById(R.id.cpf);
+        end = (EditText) findViewById(R.id.end);
+        email = (EditText) findViewById(R.id.email);
+        senha = (EditText) findViewById(R.id.senha);
+        btCad = (ImageButton) findViewById(R.id.btCad);
 
-        btn4.setOnClickListener(new View.OnClickListener(){
+        btCad.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
                 CRUD crud = new CRUD(getBaseContext());
-                String nome = et_nome.getText().toString();
-                String idade= et_idade.getText().toString();
-                String rg= et_rg.getText().toString();
-                String cpf= et_cpf.getText().toString();
-                String endereco= et_endereco.getText().toString();
-                String email= et_email.getText().toString();
-                String senha= et_senha.getText().toString();
-                String resultado=crud.inserir(nome, idade, rg, cpf, endereco, email, senha);
+
+                String nome = nm.getText().toString();
+                String RG= rg.getText().toString();
+                String CPF= cpf.getText().toString();
+                String Endereco= end.getText().toString();
+                String Email= email.getText().toString();
+                String Senha= senha.getText().toString();
+
+                String resultado=crud.inserir(nome,RG,CPF,Endereco,Email,Senha);
                 Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_LONG).show();
             }
         });
